dffdf90b3b5ea5b22322eb8165ecf83d3d822496
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-06-12 10:05:45 +0530

    fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,6 +19,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.7",
   networks: {
@@ -31,7 +33,7 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
     },
     sepolia: {
-      url: process.env.SEPOLIA_URL,
+      url: "https://eth-sepolia.g.alchemy.com/v2/demo",
       chainId: 11155111,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
